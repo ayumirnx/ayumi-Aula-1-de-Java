fa7e5c98e77fd29686f2e1def39e887214aba9ec
ayumirnx <tokitorian@gmail.com>
2024-12-21 21:20:19 +0000

    v

---
+++ main.java
@@ -0,0 +1,8 @@
+//pontos da sintaxe
+//ponto 1- toda vez que criarmos um arquivo pro java temos que declarar uma classe no java
+/*ponto 2- o nome da classe que formos declarar, tem que ter o mesmo nome do arquivo one ela se encontra,
+ex: se o nome do arquivo for "main.java" o nomde da classe sera "main", podemos tambem declarar mais uma classe no mesmo arquivo,
+mas é recomendado que tenhamos pelomenos uma classe com o mesmo nome do arquivo*/
+public class main {
+    int minhaidade = 20;
+}
